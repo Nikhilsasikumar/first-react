0ad66c2814e75395e033990a241f923f2529621b
Nikhilsasikumar <nikhilsasikumar1@gmail.com>
2020-07-14 16:36:33 -0700

    TodoApp insert and delete completed

--- src/component/TodoApp/TodoApp.js
+++ src/component/TodoApp/TodoApp.js
@@ -1,21 +1,49 @@
 import React, { Component } from 'react'
 import "./TodoApp.css";
 export default class TodoApp extends Component {
+    state={
+        input:"",
+        items:[]
+    }
+    handleChage=(event)=>{
+        this.setState({
+            input:event.target.value
+        });
+        
+    };
+    storeItems=(event)=>{
+        event.preventDefault();
+        const {input}=this.state;
+        
+        this.setState({
+            items:[...this.state.items,input],
+            input:""
+        });
+
+    }
+    deleteitems=(key)=>{
+        const allitems=this.state.items;
+        allitems.splice(key,1);
+        this.setState({
+            items:allitems
+        });
+      
+    }
     render() {
+        const {input,items}=this.state;
+        console.log(items);
         return (
             <div className='todo-container'>
                 
-                <form className='input-section'>
+                <form className='input-section' onSubmit={this.storeItems}>
                 <h1>Todo component</h1>
 
-                    <input type='text' placeholder='Enter Items...' />
+                    <input type='text' value={input} onChange={this.handleChage} placeholder='Enter Items...' />
                 </form>
                 <ul>
-                    <li>Items <i className="fa fa-trash" aria-hidden="true"></i></li>
-                    <li>Items <i className="fa fa-trash" aria-hidden="true"></i></li>
-
-                    <li>Items <i className="fa fa-trash" aria-hidden="true"></i></li>
-
+                    {items.map((data,index)=>(
+                        <li key={index}>{data}<i className="fa fa-trash" aria-hidden="true" onClick={()=>this.deleteitems(index)}></i></li>
+                    ))}
                 </ul>
             </div>
         )
