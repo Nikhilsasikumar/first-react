51f8720c2479f56cffb6fa859464a4732c6fc5ba
Nikhilsasikumar <nikhilsasikumar1@gmail.com>
2020-07-14 13:16:33 -0700

    Updates

---
+++ precache-manifest.8d3020f78e91c4b482c7dcc21765d4ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dcd46a8b983eac21ef828e0c0c159bd",
+    "url": "/first-react/index.html"
+  },
+  {
+    "revision": "9e5478284f6d1d331104",
+    "url": "/first-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0899288ff7a894c152cb",
+    "url": "/first-react/static/js/2.fdd95fb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-react/static/js/2.fdd95fb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e5478284f6d1d331104",
+    "url": "/first-react/static/js/main.8ed41168.chunk.js"
+  },
+  {
+    "revision": "d6ee40dacce02313aed2",
+    "url": "/first-react/static/js/runtime-main.516b4005.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/first-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
